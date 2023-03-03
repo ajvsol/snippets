9eef888cbf4b87fe44376bbb7fc9b785db479652
Aaron <22418751+ajvsol@users.noreply.github.com>
2023-03-03 12:04:44 +0000

    public-notes README now automatically updates with word count of logseq notes

--- logseq_public_sync.sh
+++ logseq_public_sync.sh
@@ -20,12 +20,26 @@ rsync -av "$SRC_DIR/$FILE3" "$DEST_DIR/"
 
 # Check if there are any changes
 if [[ $(git diff --name-only $FILE1 "$FILE2" "$FILE3" ) ]]; then
-#OLD
-#if git status --porcelain $FILE1 "$FILE2" "$FILE3" | grep -q "^ M"; then
-    # Commit the changes
+
+    # Get the current date and time
     DATE=$(date +"%Y-%m-%d %H:%M:%S")
+    DATE2=$(date +"%Y-%m-%d")
+
+    # Update the word count line in the README.md file
+
+    DEST_DIR_WC=$(cd "$DEST_DIR" && wc -w *.md | awk '{ print $1 }' | paste -sd+ | bc)
+    SRC_DIR_WC=$(cd "$SRC_DIR" && wc -w *.md | awk '{ print $1 }' | paste -sd+ | bc)
+    OTHER_LS_DIR="$VAULT1/Android-sync/Android-ATHENAEUM/Logseq/logseq-unenc/pages"
+    OTHER_LS_DIR_WC=$(cd "$OTHER_LS_DIR" && wc -w *.md | awk '{ print $1 }' | paste -sd+ | bc)
+
+    sed -i "s/^This repo.*/This repo contain $DEST_DIR_WC words, which represents $(awk "BEGIN { printf \"%.2f%%\", $DEST_DIR_WC / $SRC_DIR_WC * 100 }") of my coding related notes ($SRC_DIR_WC words), and $(awk "BEGIN { printf \"%.2f%%\", $DEST_DIR_WC / ($OTHER_LS_DIR_WC + $SRC_DIR_WC) * 100 }") of my total notes ($OTHER_LS_DIR_WC words). Last updated: $DATE. /g" README.md
+
+    # Change to the destination directory
+    cd $DEST_DIR
+
+    # Commit the changes
+    
     git add $FILE1 "$FILE2" "$FILE3"
     git commit -m "Sync files at $DATE"
-    git pull
     git push
 fi
